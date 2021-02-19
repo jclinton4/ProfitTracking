0ff0522ba0b3b152e4cb9163c423faefbda94e8a
Joshua Clinton <Joshuac@ami.com>
2021-02-18 22:02:21 -0500

    Add react frontend

--- trades/models.py
+++ trades/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Trades(models.Model):
+    # Need to determine what fields I want here
+    symbol = models.CharField("Symbol", max_length=255)
+
+    def __str__(self):
+        return self.symbol
